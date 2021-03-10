4c09aeaea5b14b28b76fe608f5761920627508de
Ravi Mohan <ravimohan1991@gmail.com>
2021-03-10 08:51:07 +0530

    Added SPIRV in premake

--- premake5.lua
+++ premake5.lua
@@ -149,3 +149,59 @@ project "HLSL"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
+
+project "SPIRV"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+
+	targetdir ("build/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"SPIRV/GlslangToSpv.cpp",
+		"SPIRV/InReadableOrder.cpp",
+		"SPIRV/Logger.cpp",
+		"SPIRV/SpvBuilder.cpp",
+		"SPIRV/SpvPostProcess.cpp",
+		"SPIRV/doc.cpp",
+		"SPIRV/SpvTools.cpp",
+		"SPIRV/disassemble.cpp",
+		"SPIRV/CInterface/spirv_c_interface.cpp"
+	}
+
+	includedirs
+	{
+		"../glslang",
+		"../SPIRV"
+	}
+
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+		files
+		{
+			"glslang/OSDependent/Windows/ossource.cpp"
+		}
+
+	filter "system:linux"
+		-- https://github.com/premake/premake-core/wiki/pic
+		pic "On"
+		buildoptions "-std=c++11"
+		systemversion "latest"
+
+		files
+		{
+			"glslang/OSDependent/Unix/ossource.cpp"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
