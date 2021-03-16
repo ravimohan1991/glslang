1cc22d23d9e285d85c7b3aa2c03ab6f1ace3700c
Ravi Mohan <ravimohan1991@gmail.com>
2021-03-16 09:16:38 +0530

    Added glslang dependencies build in premake

--- premake5.lua
+++ premake5.lua
@@ -41,6 +41,128 @@ project "OSDependent"
 		runtime "Release"
 		optimize "on"
 
+project "GenericCodeGen"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+
+	targetdir ("build/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"glslang/GenericCodeGen/CodeGen.cpp",
+		"glslang/GenericCodeGen/Link.cpp"
+	}
+
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+		files
+		{
+			"glslang/OSDependent/Windows/ossource.cpp"
+		}
+
+	filter "system:linux"
+		-- https://github.com/premake/premake-core/wiki/pic
+		pic "On"
+		buildoptions "-std=c++11"
+		systemversion "latest"
+
+		files
+		{
+			"glslang/OSDependent/Unix/ossource.cpp"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+project "MachineIndependent"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+
+	targetdir ("build/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"glslang/MachineIndependent/glslang.m4",
+		"glslang/MachineIndependent/glslang.y",
+		"glslang/MachineIndependent/glslang_tab.cpp",
+		"glslang/MachineIndependent/attribute.cpp",
+		"glslang/MachineIndependent/Constant.cpp",
+		"glslang/MachineIndependent/iomapper.cpp",
+		"glslang/MachineIndependent/InfoSink.cpp",
+		"glslang/MachineIndependent/Initialize.cpp",
+		"glslang/MachineIndependent/IntermTraverse.cpp",
+		"glslang/MachineIndependent/Intermediate.cpp",
+		"glslang/MachineIndependent/ParseContextBase.cpp",
+		"glslang/MachineIndependent/ParseHelper.cpp",
+		"glslang/MachineIndependent/PoolAlloc.cpp",
+		"glslang/MachineIndependent/RemoveTree.cpp",
+		"glslang/MachineIndependent/Scan.cpp",
+		"glslang/MachineIndependent/ShaderLang.cpp",
+		"glslang/MachineIndependent/SymbolTable.cpp",
+		"glslang/MachineIndependent/Versions.cpp",
+		"glslang/MachineIndependent/intermOut.cpp",
+		"glslang/MachineIndependent/limits.cpp",
+		"glslang/MachineIndependent/linkValidate.cpp",
+		"glslang/MachineIndependent/parseConst.cpp",
+		"glslang/MachineIndependent/reflection.cpp",
+		"glslang/MachineIndependent/preprocessor/Pp.cpp",
+		"glslang/MachineIndependent/preprocessor/PpAtom.cpp",
+		"glslang/MachineIndependent/preprocessor/PpContext.cpp",
+		"glslang/MachineIndependent/preprocessor/PpScanner.cpp",
+		"glslang/MachineIndependent/preprocessor/PpTokens.cpp",
+		"glslang/MachineIndependent/propagateNoContraction.cpp"
+	}
+
+	includedirs
+	{
+		"../glslang"
+	}
+
+	links
+	{
+		"GenericCodeGen"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+		files
+		{
+			"glslang/OSDependent/Windows/ossource.cpp"
+		}
+
+	filter "system:linux"
+		-- https://github.com/premake/premake-core/wiki/pic
+		pic "On"
+		buildoptions "-std=c++11"
+		systemversion "latest"
+
+		files
+		{
+			"glslang/OSDependent/Unix/ossource.cpp"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
 
 project "OGLCompiler"
 	kind "StaticLib"
@@ -93,6 +215,13 @@ project "glslang"
 	{
 		"../glslang",
 		"StandAlone"
+	}
+
+	links
+	{
+		"OGLCompiler",
+		"OSDependent",
+		"MachineIndependent"
 	}
 
 	filter "system:windows"
